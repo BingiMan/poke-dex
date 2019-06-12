3e92ee1a06cbac39321c4f8c5db203f651ce83c9
Luis Carbonel <bingi@Luiss-MacBook-Pro.local>
2019-06-11 22:33:24 -0400

    poke-dex

--- pokedex.js
+++ pokedex.js
@@ -1,52 +1,71 @@
 const domain = 'https://pokeapi.co/api/v2';
 const search = '/pokemon/';
-//ULR/////////////////////////////////////////////////
-
 const searchBase = `${domain}${search}`;
 const listLimit = '?offset=1&limit=800'
+//ULR/////////////////////////////////////////////////
+
+
+// Element Constants instances
 const buttonSearch = document.querySelector('#search-button');
+const randomSearch = document.querySelector('#random-search');
+
 const inputPokemon = document.querySelector('#pokemon-search');
 
+const gobalContainerList = document.querySelector('#display-list');
+// const pokemonSearched = document.querySelector('#pokemon-list')
+
+const pokemonDetailsContainer = document.querySelector('.pokemon-details');
+const pokemonNameElement = document.querySelector('#pokemon-name-container > span');
+const pokemonDetailListContainer = document.querySelector('#pokemon-details-container > ul');
+const pokemonImgContainer = document.querySelector('#pokemon-img-container');
 //SEARCH BUTTON////////////////////////////////////////
 
-buttonSearch.addEventListener('click', async () => {
-  const inputValue = inputPokemon.value;
+function clearPokemonDetails() {
+  pokemonImgContainer.innerHTML = '';
+  pokemonNameElement.innerHTML = '';
+  pokemonDetailListContainer.innerHTML = '';
+}
+
+function renderPokemonDetails() {
+  clearPokemonDetails();
+
   let response = await axios.get(searchBase + inputValue);
-
   let poke = response.data;
-
   console.log(poke);
 
-  const pokemonSearched = document.querySelector('#pokemon-list')
-  pokemonSearched.innerHTML = '';
-  let pokemonItem = document.createElement('div');
-  let pokemonName = document.createElement('p');
-  pokemonName.classList.add('bold-text');
+  pokemonNameElement.classList.add('bold-text');
 
-  let pokemonDetails = document.createElement('ul');
   let pokemonImg = document.createElement('img');
 
-  pokemonName.innerHTML = `Name: ${poke.name}`;
+  pokemonNameElement.innerHTML = poke.name;
+
   for (let i = 0; i < poke.abilities.length; i++) { // looping through abilities length
     let pokemonAbilities = document.createElement('li'); // creating one 'li' per ability
     pokemonAbilities.innerHTML = poke.abilities[i].ability.name; // creating the li with the name of the ability
-    pokemonDetails.appendChild(pokemonAbilities); // inserting the 'li' into the container 'lu'
+    pokemonDetailListContainer.appendChild(pokemonAbilities); // inserting the 'li' into the container 'lu'    
   }
   pokemonImg.setAttribute('src', poke.sprites.front_default);
   pokemonImg.setAttribute('alt', poke.name);
 
-  pokemonItem.appendChild(pokemonImg);
-  pokemonItem.appendChild(pokemonName);
-  pokemonItem.appendChild(pokemonDetails);
+  pokemonImgContainer.appendChild(pokemonImg);
 
+  displayDetails(pokemonDetailsContainer);
+}
+buttonSearch.addEventListener('click', async () => {
+  const inputValue = inputPokemon.value;
 
-  pokemonSearched.appendChild(pokemonItem);
-  console.log(pokemonSearched);
+  renderPokemonDetails();
 });
 
 
+function displayDetails(el) {
+  console.log(el.getAttribute('data-active'))
+  if (el.getAttribute('data-active') === '0') {
+    el.setAttribute('data-active', '1');
+  }
+}
+
 //RANDOM SEARCH BUTTON/////////////////////////////////////////
-const randomSearch = document.querySelector('#random-search');
 
 randomSearch.addEventListener('click', async () => {
   let randomPokemon = Math.floor(Math.random() * 807);
@@ -85,7 +104,6 @@ randomSearch.addEventListener('click', async () => {
 });
 
 ////DISPLAY LIST/////////////////////////////////////////
-const gobalContainerList = document.querySelector('#display-list');
 
 let displayPokemonList = async () => {
   let containerPokemonList = document.createElement('ul');
@@ -103,16 +121,6 @@ let displayPokemonList = async () => {
 }
 
 displayPokemonList();
-
-// const gobalContainerList = document.querySelector('#display-list');
-
-// let displayPokeList = async () => {
-//   let listedPokemon
-//   let containerPokemonList = document.createElement('ul');
-//   let response = await axios.get(searchBase);
-//   let listedPokemon = response.data;
-// }
-// console.log(listedPokemon);
 
 
 
